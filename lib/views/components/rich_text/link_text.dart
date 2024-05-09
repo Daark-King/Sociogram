4e86335493d8689b5186f49852104322c03cadc6
Ankit Jangid <1ms21cs019@msrit.edu>
2024-05-09 20:16:50 +0530

    login and Animation done

---
+++ lib/views/components/rich_text/link_text.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart' show immutable, VoidCallback;
+import 'package:sociogram/views/components/rich_text/base_text.dart';
+
+@immutable
+class LinkText extends BaseText {
+  final VoidCallback onTapped;
+  const LinkText({
+    required super.text,
+    required this.onTapped,
+    super.style,
+  });
+}
